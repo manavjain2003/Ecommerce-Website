1463442f1d86a3efc4c5e39ebd687cf210ab1af3
Manav jain <169134003+manavjain2003@users.noreply.github.com>
2025-01-27 21:04:58 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,30 @@
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Hero from "./pages/Hero";
+import Login from "./pages/Login";
+import Registeration from "./pages/Registeration";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Products from  "./pages/Products";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import AddtoCart from "./pages/AddtoCart";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Hero />}/>
+        <Route path="/products" element={<Products />} />
+        <Route path="/register" element={<Registeration />} />
+        <Route path="/login" element={< Login/>} />
+        <Route path="/about" element={< About/>} />
+        <Route path="/dashboard" element={< Dashboard/>} />
+        <Route path="/cart" element={< AddtoCart/>} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
